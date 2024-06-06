ed319089148c98e5c438646d3545fc3a968c6a72
tobias1012 <tobias1012@gmail.com>
2024-06-06 20:56:35 +0200

    Added poetry support for python deps

--- flake.nix
+++ flake.nix
@@ -2,8 +2,9 @@
   description = "CTF flake template";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
       deps = with pkgs;[
@@ -12,12 +13,20 @@
 	  pwncat
 	  pwndbg
       ];
+
+      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv;
+      pythonEnv = mkPoetryEnv {
+	python = pkgs.python311;
+	projectDir = ./.;
+      };
+
       fhs = pkgs.buildFHSUserEnv {
 	name = "fhs-shell";
-	targetPkgs = pkgs: deps;};
+	targetPkgs = pkgs: deps ++ [pythonEnv];
+      };
     in {
       devShells.default = pkgs.mkShell {
-        packages = deps;
+        packages = deps ++ [pythonEnv];
 	shellHook = ''
 	'';
       };
